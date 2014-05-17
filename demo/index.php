6fa720a3f1278cbad41841e5362ac2245dddd5e4
mingren <mingren@mogujie.com>
2014-05-17 16:38:56 +0800

    create demo

---
+++ demo/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zuoca
+ * Date: 5/17/14
+ * Time: 4:31 PM
+ */
+
+$return=array('name'=>'zuoca',
+'sex'=>'male');
+
+
+$result=json_encode($return);
+
+echo $result;
+
